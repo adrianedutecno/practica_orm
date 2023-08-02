c58b5c013ba2da79cf9e25edbed132b49b1f5ce1
afranco <adrian.franco@edutecno.com>
2023-08-02 19:45:54 -0400

    add db

--- config/config/settings.py
+++ config/config/settings.py
@@ -89,6 +89,10 @@ DATABASES = {
         'PASSWORD': 'admin',  # password user database
         'HOST': '127.0.0.1',  # localhost
         'PORT': '5432',  # database port number
+    },
+    'local': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
